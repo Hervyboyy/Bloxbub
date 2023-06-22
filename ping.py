9a4a526083d57d5f76a1635bea1694be63f6cca5
Hervyboyy <137232930+Hervyboyy@users.noreply.github.com>
2023-06-22 13:05:30 -0400

    Add files via upload

---
+++ ping.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+
+class Ping(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+        @commands.Cog.listner()
+        async def on_ready(self):
+            print("Ping.py Is ready!")
+
+            @commands.command()
+            async def ping(ctx):
+             bot_latency = round(client.latency * 1000)
+
+             await ctx.send(f"Pong! {bot_latency} ms.")
+
+             async def setup(client):
+                await client.add_cog(Ping{client})
+
+                async def main():
+                   async with client:
+                      await load()
+                      await client.start("MTEyMTIyMDg3ODk4MjcyMTYzNg.GbRovN.jEgU6bcvVaHvL62r0lqnMJAMoLq58lspXg9KFY")
+
+                      
