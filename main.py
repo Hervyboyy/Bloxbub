4198eb0c07f2c3dd36a29a9f7e8993d38d8527a5
Hervyboyy <137232930+Hervyboyy@users.noreply.github.com>
2023-06-22 13:04:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,54 @@
-import nextcord
-import config
-import os
-from nextcord.ext import commands
+import random
+from _ast import List
+from discord.ext import commands, tasks
+from itertools import cycle
+import discord
+from discord.ext import commands
 
-Intents = nextcord.Intents.all()
-Intents.members = True
 
-bot = commands.Bot(command_prefix='/'),
+client = commands.Bot(command_prefix="!" , intents=discord.Intents.all())
 
-@bot.event
+@client.event
 async def on_ready():
-    print('Logged in as {bot.user.name} ({bot.user.id})')
-    print(_______________________________________________)
+    print("Sucess Bot is connected to Discord ")
 
 
+@client.command()
+async def magic_eightball(ctx, *, question):
+    with open("../respones.txt", "r") as f:
+        random_response: List[str] = f.readlines()
+        response: object = random.choice(random_response)
+
+        await ctx.send(response)
+
+
+bot_status = cycle({"Type !help for commands", "BETA BOT", "Timezone EST", "Have a good day!"})
+
+@tasks.loop(seconds=10)
+async def change_status():
+    await client.change_presence(activity=discord.Game(next(bot_status)))
+
+@client.event
+async def on_ready():
+    print("Success: Bot is connected to Discord")
+    change_status.start()
+
+
+async def load():
+   for filename in os.listdir("./cogs"):
+       if filename.endswith(".py"):
+           await client.load_extension(f"cogs.{filename[:-3]}")
+
+
+           async def main():
+               async with client:
+                   await load()
+                   await client.start("MTEyMTIyMDg3ODk4MjcyMTYzNg.GbRovN.jEgU6bcvVaHvL62r0lqnMJAMoLq58lspXg9KFY")
+
+asyncio.run(main{})
+
+
+client.run("MTEyMTIyMDg3ODk4MjcyMTYzNg.GbRovN.jEgU6bcvVaHvL62r0lqnMJAMoLq58lspXg9KFY")
 
 
 
